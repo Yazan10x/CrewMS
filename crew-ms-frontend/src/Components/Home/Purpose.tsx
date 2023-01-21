1b75e15342109e644a25eb9c6a7237fe0ed29a5c
ShaarifAliSyed <shaarifalisyed@outlook.com>
2023-01-21 18:06:13 -0500

    Home Page ver1 complete

--- crew-ms-frontend/src/Components/Home/Purpose.tsx
+++ crew-ms-frontend/src/Components/Home/Purpose.tsx
@@ -4,89 +4,43 @@ import {
     chakra,
     Container,
     Flex,
-    Icon, Text,
-    SimpleGrid, Stack,
-    useColorModeValue, Heading,
+    Icon,
+    Text,
+    SimpleGrid,
+    Stack,
+    useColorModeValue,
+    Heading,
+    VStack,
+    Card,
+    Stat,
+    StatLabel,
+    StatNumber,
+    StatHelpText,
 } from '@chakra-ui/react';
 import * as React from "react";
+import {PieChart} from "./PieChart";
+import {RadarChart} from "./RadarChart";
+import {PieCard} from "./PieCard";
+import {RadarCard} from "./RadarCard";
+import {UsersAPI} from "../../APIs/UsersAPI";
+import {useEffect, useState} from "react";
 
-const testimonials = [
-  {
-    title: 'Crew',
-    content:
-      'Crew',
-    avatar:
-      '',
-  },
-];
 
-const backgrounds = [
-  `url("data:image/svg+xml, %3Csvg xmlns=\'http://www.w3.org/2000/svg\' width=\'560\' height=\'185\' viewBox=\'0 0 560 185\' fill=\'none\'%3E%3Cellipse cx=\'102.633\' cy=\'61.0737\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23ED64A6\' /%3E%3Cellipse cx=\'399.573\' cy=\'123.926\' rx=\'102.633\' ry=\'61.0737\' fill=\'%23F56565\' /%3E%3Cellipse cx=\'366.192\' cy=\'73.2292\' rx=\'193.808\' ry=\'73.2292\' fill=\'%2338B2AC\' /%3E%3Cellipse cx=\'222.705\' cy=\'110.585\' rx=\'193.808\' ry=\'73.2292\' fill=\'%23ED8936\' /%3E%3C/svg%3E")`,
-  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='457.367' cy='123.926' rx='102.633' ry='61.0737' transform='rotate(-180 457.367 123.926)' fill='%23ED8936'/%3E%3Cellipse cx='160.427' cy='61.0737' rx='102.633' ry='61.0737' transform='rotate(-180 160.427 61.0737)' fill='%2348BB78'/%3E%3Cellipse cx='193.808' cy='111.771' rx='193.808' ry='73.2292' transform='rotate(-180 193.808 111.771)' fill='%230BC5EA'/%3E%3Cellipse cx='337.295' cy='74.415' rx='193.808' ry='73.2292' transform='rotate(-180 337.295 74.415)' fill='%23ED64A6'/%3E%3C/svg%3E")`,
-  `url("data:image/svg+xml, %3Csvg xmlns='http://www.w3.org/2000/svg' width='560' height='185' viewBox='0 0 560 185' fill='none'%3E%3Cellipse cx='102.633' cy='61.0737' rx='102.633' ry='61.0737' fill='%23ED8936'/%3E%3Cellipse cx='399.573' cy='123.926' rx='102.633' ry='61.0737' fill='%2348BB78'/%3E%3Cellipse cx='366.192' cy='73.2292' rx='193.808' ry='73.2292' fill='%230BC5EA'/%3E%3Cellipse cx='222.705' cy='110.585' rx='193.808' ry='73.2292' fill='%23ED64A6'/%3E%3C/svg%3E")`,
-];
+export const Purpose = () => {
 
-interface TestimonialCardProps {
-  title: string;
-  content: string;
-  index: number;
-}
+    const [count, setCount] = useState<string>("")
 
-function TestimonialCard(props: TestimonialCardProps) {
-  const { title, content, index } = props;
-  return (
-    <Flex
-      boxShadow={'lg'}
-      maxW={'700px'}
-      direction={{ base: 'column-reverse', md: 'row' }}
-      width={'full'}
-      rounded={'xl'}
-      p={10}
-      justifyContent={'space-between'}
-      position={'relative'}
-      bg={useColorModeValue('white', 'gray.800')}
-      _after={{
-        content: '""',
-        position: 'absolute',
-        height: '21px',
-        width: '29px',
-        left: '35px',
-        top: '-10px',
-        backgroundSize: 'cover',
-      }}
-      _before={{
-        content: '""',
-        position: 'absolute',
-        zIndex: '-1',
-        height: 'full',
-        maxW: '640px',
-        width: 'full',
-        filter: 'blur(40px)',
-        transform: 'scale(0.98)',
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: 'cover',
-        top: 0,
-        left: 0,
-        backgroundImage: backgrounds[index % 4],
-      }}>
-      <SimpleGrid  columns={[null, 1]} spacing='30px'>
-          <Text fontSize='xg' as='b' color='green.400'>{title}</Text>
-          <Flex
-          direction={'column'}
-          textAlign={'left'}
-          justifyContent={'space-between'}>
-          <chakra.p
-            fontSize={'md'}
-            pb={4}>
-            {content}
-          </chakra.p>
-        </Flex>
-      </SimpleGrid>
-    </Flex>
-  );
-}
+    const get_user_count = () => {
+        UsersAPI.get_users().then((res) => {
+            let data = res!
+            setCount(data.length.toString())
+        })
+    }
 
-export default function GridBlurredBackdrop() {
+    useEffect(() => {
+        get_user_count()
+    }, []);
+
   return (
     <Flex
       textAlign={'center'}
@@ -94,28 +48,62 @@ export default function GridBlurredBackdrop() {
       justifyContent={'center'}
       direction={'column'}
       width={'full'}>
-        <Stack as={Box} textAlign={'center'} spacing={{ base: 8, md: 14 }} py={{ base: 20, md: 36 }}>
+        <Stack as={Box} textAlign={'center'} spacing={{ base: 8, md: 14 }} py={{ base: 25, md: 36 }}>
+            <VStack align="stretch">
+                <PieCard></PieCard>
+                <RadarCard></RadarCard>
+            </VStack>
           <Heading
             fontWeight={600}
             fontSize={{ base: '3xl', sm: '4xl', md: '5xl' }}
             lineHeight={'110%'}>
-            We are an Crew Ship! <br />
+            Here is a quick update on our Crewsade! <br />
           </Heading>
-            <Box>
-                <chakra.h2 margin={'auto'} width={'70%'} alignContent={'center'} color={useColorModeValue('gray.500', 'gray.400')}>
-          See why over{' '} <chakra.strong color={useColorModeValue('gray.700', 'gray.50')}> 100+</chakra.strong>{' '}
-          Crew! </chakra.h2>
-            </Box>
         </Stack>
       <SimpleGrid
-        columns={{ base: 1, xl: 1 }}
+        columns={[2, null, 2]}
         spacing={'20'}
         mt={16}
         mx={'auto'}>
-        {testimonials.map((cardInfo, index) => (
-          <TestimonialCard {...cardInfo} index={index} />
-        ))}
+          <Card
+              variant = 'elevated'
+              minW={"400px"}
+              >
+              <Stat>
+                  <StatLabel fontSize={'3xl'}>Number of Members</StatLabel>
+                  <StatNumber>{count}</StatNumber>
+                  <StatHelpText>Present time</StatHelpText>
+              </Stat>
+          </Card>
+          <Card
+              variant = 'elevated'
+              >
+              <Stat>
+                  <StatLabel fontSize={'3xl'}>Type of Exploration</StatLabel>
+                  <StatNumber>Space Exploration</StatNumber>
+                  <StatHelpText>Journey beyond the milky way!</StatHelpText>
+              </Stat>
+          </Card>
+          <Card
+              variant = 'elevated'
+              >
+              <Stat>
+                  <StatLabel fontSize={'3xl'}>Mode of Transportation</StatLabel>
+                  <StatNumber>Space Shuttle</StatNumber>
+                  <StatHelpText>A NASA Project</StatHelpText>
+              </Stat>
+          </Card>
+          <Card
+              variant = 'elevated'
+              >
+              <Stat>
+                  <StatLabel fontSize={'3xl'}>Travel Speed</StatLabel>
+                  <StatNumber>28,000</StatNumber>
+                  <StatHelpText>km/hr</StatHelpText>
+              </Stat>
+          </Card>
       </SimpleGrid>
     </Flex>
   );
 }
+
---
+++ crew-ms-frontend/src/Components/Home/RadarCard.tsx
@@ -0,0 +1,77 @@
+import {
+    Button,
+    Card,
+    CardBody,
+    CardFooter,
+    Heading,
+    Stack,
+    Text,
+    Image, Box, Center, LightMode
+} from "@chakra-ui/react";
+import React from 'react';
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Radar } from 'react-chartjs-2';
+import {RadarChart} from "./RadarChart";
+import {GiBulletImpacts} from "react-icons/all";
+
+ChartJS.register(
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend
+);
+
+export const RadarCard = () => {
+    return (
+        <LightMode>
+            <Card
+                  // direction={{ base: 'column', sm: 'row' }}
+                  overflow='hidden'
+                  variant='outline'
+                  align='center'
+            >
+              <Stack>
+                <CardBody>
+                    <Box>
+                            <Heading size='md'>Average Crew Statistics</Heading>
+                            <Text>
+                                The following graph shows the average statistics of all crew members, in the following categories
+                            </Text>
+                        <li>
+                            The number of hours of sleep crew members get on a daily basis
+                        </li>
+                        <li>
+                            The number of meals the crew members have on a daily basis
+                        </li>
+                        <li>
+                            The number of hours crew members engage in physical exersice
+                        </li>
+                        <li>
+                            The average level of workplace organization by the crew members. (This is ranked from 1-5 where 1 is very unorganized and 5 is very organized).
+                        </li>
+                        <Center>
+                            <Box width='800px'>
+                                <RadarChart></RadarChart>
+                            </Box>
+                        </Center>
+                    </Box>
+                </CardBody>
+                {/*<Box p={5} shadow='md' borderWidth='1px'>*/}
+                {/*        /!*<Heading fontSize='xl'>{"Crew's Diversity"}</Heading>*!/*/}
+                {/*        <Text mt={4}><RadarChart></RadarChart></Text>*/}
+                {/*    </Box>*/}
+              </Stack>
+            </Card>
+        </LightMode>
+    )
+}
