8c439d62d8546c7f52fb67534cd79d6cdde8feba
Usman Tahir <65866570+UsmanTahir7@users.noreply.github.com>
2023-01-21 14:10:02 -0500

    finished incident table

---
+++ crew-ms-frontend/src/APIs/IncidentsAPI.ts
@@ -0,0 +1,30 @@
+import FLASK_HTTPS from "./FLASK_API";
+import {ObjectID} from "bson";
+import {User} from "../Models/User";
+import { Incident } from "../Models/Incident";
+
+export namespace IncidentsAPI {
+
+    let route_name = "/incidents"
+
+    export const get_incident = async (incident_id: ObjectID) => {
+        return FLASK_HTTPS.get(route_name + "/get_incident/" + incident_id.toString())
+            .then((res) => {
+                return res.data as Incident
+            })
+            .catch((res) => {
+                console.log(res)
+            })
+    }
+
+    export const get_incidents_by = async (query: Map<string, string>) => {
+        return FLASK_HTTPS.post(route_name + "/get_incidents_by",
+            query)
+            .then((res) => {
+                return res.data as Array<Incident>
+            }).catch((res) => {
+                console.log(res)
+            })
+    }
+
+}
