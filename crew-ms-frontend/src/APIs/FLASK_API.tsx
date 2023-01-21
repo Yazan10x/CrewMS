a67e0f3a225747674b9cf59d1c92a54100f46b6e
Yazan Armoush <yazan.armoush1@gmail.com>
2023-01-21 10:13:47 -0500

    Fixed Public API

--- crew-ms-frontend/src/APIs/FLASK_API.tsx
+++ crew-ms-frontend/src/APIs/FLASK_API.tsx
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const FLASK_API_ADDRESS: string = "https://flask.usec.club";
+const FLASK_API_ADDRESS: string = "https://flaskapi.usec.club";
 const FLASK_LOCAL_ADDRESS: string = "http://127.0.0.1:5000"
 
 const instance = axios.create({
-  baseURL: FLASK_LOCAL_ADDRESS,
+  baseURL: FLASK_API_ADDRESS,
   headers: {
     'Content-Type': 'application/json',
   }
