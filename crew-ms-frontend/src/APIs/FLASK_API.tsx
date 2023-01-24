10823d17b00d741ce348c517a05f0ca1546ed0e3
Yazan Armoush <yazan.armoush1@gmail.com>
2023-01-23 23:35:58 -0500

    Update FLASK_API.tsx

--- crew-ms-frontend/src/APIs/FLASK_API.tsx
+++ crew-ms-frontend/src/APIs/FLASK_API.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const FLASK_API_ADDRESS: string = "https://flaskapi.usec.club";
+const FLASK_API_ADDRESS: string = "https://crewiseapi.usec.club";
 const FLASK_LOCAL_ADDRESS: string = "http://127.0.0.1:5000"
 
 const instance = axios.create({
